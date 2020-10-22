6fae89e43cec96b29091e9443ce94ac1190df704
eudoxos <eu@doxos.eu>
2020-10-22 19:07:51 +0200

    use DHCP discover broadcast

--- broad.c
+++ broad.c
@@ -15,6 +15,40 @@ void set_buffer_size(GPollFD* gpfd, int bufsize){
 	#endif
 	if(result!=0) g_critical("setsockopt failed.");
 }
+#if 0
+// DHCP discover packet
+char payload[]={
+/* op, htype, hlen, hops */ 0x01,0x01,0x06,0x00, 
+/* id */ 0xaa,0xbb,0xcc,0xdd, 
+/* secs, reserved */ 0,0,0,0, 
+/* client ip */ 0,0,0,0,
+/* your ip */   0,0,0,0,
+/* server ip */ 0,0,0,0,
+/* relay ip */  0,0,0,0,
+/* MAC */ 0xaa,0xbb,0xcc,0xdd,0xee,0xff, 
+/* padding */ 0,0,0,0, 0,0,0,0, 0,0, 
+/* 64b: server hostname */
+0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 
+/* 128b: bootp filename */
+0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 
+/* magic cookie */ 0x63,0x82,0x53,0x63,
+/* option: discover */ 0x35,0x01,0x03,
+/* end */ 0xff,
+0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0
+};
+const int dst_port=67;
+const int listen_port=68;
+#else
+// GVCP discover packet
+const char payload[]={/*type*/0x42,/*flags*/0x01,/*command*/0x00,0x02,/*size*/0x00,0x00,/*id*/0xff,0xff};
+const int listen_port=3956;
+//const int dst_port=3956;
+#endif
 
 /* there is bunch of leaks in here, I know */
 int main(int argc, char** argv){
@@ -32,22 +66,23 @@ int main(int argc, char** argv){
 	fds[0].revents=0;
 	set_buffer_size(&fds[0],256*1024);
 	/* bind to the interface */
-	GSocketAddress *addr=g_inet_socket_address_new_from_string(argv[1],/*disregarded*/0);
+	GSocketAddress *addr=g_inet_socket_address_new_from_string(argv[1],listen_port);
 	if(!addr) { g_critical("parsing IP address %s failed.",argv[1]); return 1; }
 	result=g_socket_bind(sock,addr,FALSE,&error);
 	if(!result){ g_critical("g_socket_bind failed: %s",error->message); return 1; }
 
+#if 0
 	set_broadcast(sock,TRUE);
-	/* this is gvcp (GigEVision control protocol) discovery packet, port 3956 */
-	GSocketAddress* baddr = g_inet_socket_address_new_from_string("255.255.255.255",3956);
-	const char packet[]={/*type*/0x42,/*flags*/0x01,/*command*/0x00,0x02,/*size*/0x00,0x00,/*id*/0xff,0xff};
-	g_socket_send_to(sock,baddr,&packet[0],sizeof(packet),NULL,&error);
+	/* send discovery packet */
+	GSocketAddress* baddr = g_inet_socket_address_new_from_string("255.255.255.255",dst_port);
+	g_socket_send_to(sock,baddr,&payload[0],sizeof(payload),NULL,&error);
 	if(error){ g_critical("g_socket_send failed: %s",error->message); return 1; }
-	fprintf(stderr,"Sent %d bytes discover to 255.255.255.255 over %s\n",(int)sizeof(packet),argv[1]);
+	fprintf(stderr,"Sent %d bytes discover to 255.255.255.255 over %s\n",(int)sizeof(payload),argv[1]);
 	set_broadcast(sock,FALSE);
+#endif
 
-	/* poll sockets (just one, really) to receive reply */
-	result=g_poll(&fds[0],1,1000);
+	/* poll sockets (just one, really) for 10s to receive reply */
+	result=g_poll(&fds[0],1,10000);
 
 	if(result==0){ g_critical("g_poll timed out."); exit(1); }
 	else if(result<0){ g_critical("g_poll returned %d (error or call interrupted)",result); return 1; }
